04ad6155938e8cae27b598d2e2359d085ea8878f
LWMEPOST <wmxx2857213439@163.com>
2026-03-03 19:41:56 +0800

    add frontend and backend configuration files

---
+++ frontend/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  transpileDependencies: true,
+  devServer: {
+    port: 8081,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src')
+      }
+    }
+  }
+};
